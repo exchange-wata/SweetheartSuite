ff63bff0ba1452ecaf09eccb3c6b29423ceae760
exchange-wata <exchange.wata0907@gmail.com>
2024-05-25 17:46:02 +0900

    guardとdecorator実装途中

--- backend/src/context/Auth/usecase/jwtAuth.usecase.ts
+++ backend/src/context/Auth/usecase/jwtAuth.usecase.ts
@@ -10,6 +10,13 @@ export class JwtAuthUsecase {
   constructor(private readonly jwtService: JwtService) {}
 
   async generateToken(input: AuthType): Promise<string> {
+    console.log('=====================================');
+    console.log(this.jwtService.sign({ userId: input.id }));
+    console.log('=====================================');
     return this.jwtService.sign({ userId: input.id });
+  }
+
+  async verifyToken(token: string) {
+    return this.jwtService.verifyAsync(token);
   }
 }
--- backend/src/context/User/adapters/resolver/request.resolver.ts
+++ backend/src/context/User/adapters/resolver/request.resolver.ts
@@ -1,16 +1,20 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { SendRequestUsecase } from '../../usecase/sendRequest.usecase';
+import { UseGuards } from '@nestjs/common';
+import { jwtAuthGuard } from 'src/context/Auth/guard/jwtAuth.guard';
+import { User } from '../../decorator/user.decorator';
 
 @Resolver()
+@UseGuards(jwtAuthGuard)
 export class RequestResolver {
   constructor(private readonly sendRequestUsecase: SendRequestUsecase) {}
 
   @Mutation(() => Boolean)
   async sendRequest(
+    @User() user,
     @Args('mailaddress') mailaddress: string,
   ): Promise<Boolean> {
-    // TODO: 仮の送信者メールアドレス(seederにあるやつを設定)
-    const senderMailaddress = 'email1@gmail.com';
+    const senderMailaddress = user.mailaddress;
     return this.sendRequestUsecase.execute(senderMailaddress, mailaddress);
   }
 }
