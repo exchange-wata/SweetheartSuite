472506a2323cdf017450f41fda293f9fc14a1a1a
Nowe-melancholy <jackpot88230021@gmail.com>
2024-05-26 16:43:26 +0900

    見た目をちょっとかっこよくした

--- frontend/app/signIn/page.tsx
+++ frontend/app/signIn/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 import { signIn } from 'next-auth/react';
 
 export default function Login() {
   return (
-    <div>
-      <h1>ログイン</h1>
-      <Button onClick={() => signIn('google')}>Googleでログイン</Button>
+    <div className="flex items-center justify-center m-20">
+      <Card className="w-[350px] h-[350px] flex items-center justify-center">
+        <CardContent>
+          <Button onClick={() => signIn('google')}>Googleでログイン</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
