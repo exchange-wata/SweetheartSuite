cb5bda4126c0aae9dbb587e8617533efd80177fb
Nowe-melancholy <jackpot88230021@gmail.com>
2024-08-03 22:13:30 +0900

    認可の実装案

--- backend/src/context/List/decorator/contents.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common';
-import { ContentsAuthGuard } from '../guard/contents.guard';
-
-export function ContentsAuth() {
-  return applyDecorators(UseGuards(ContentsAuthGuard));
-}
--- backend/src/context/List/guard/contents.guard.ts
+++ backend/src/context/List/guard/contents.guard.ts
@@ -1,61 +1,23 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { GetContentsByIdsUsecase } from '../usecase/getContentsByIds.usecase';
 import { GetListByIdUsecase } from '../usecase/getListById.usecase';
 
 @Injectable()
-export class ContentsAuthGuard implements CanActivate {
+export class ContentsAuth {
   constructor(
     private readonly getContentsByIdsUsecase: GetContentsByIdsUsecase,
     private readonly getListByIdUsecase: GetListByIdUsecase,
   ) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const ctx = GqlExecutionContext.create(context);
-    const request = ctx.getContext().req;
-    const coupleId = request.user?.coupleId;
-    const { id: contentId, ids: contentIds } = request.body?.variables ?? {};
-
-    if (contentId) {
-      return this.checkSingleContent(coupleId, contentId);
-    }
-
-    if (contentIds && contentIds.length > 1) {
-      return this.checkMultipleContents(coupleId, contentIds);
-    }
-
-    return false;
-  }
-
-  private async checkSingleContent(
-    coupleId: string,
-    contentId: string,
-  ): Promise<boolean> {
-    const contents = await this.getContentsByIdsUsecase.execute([contentId]);
-    const listId = contents[0]?.listId;
-
-    if (!listId) {
-      throw new Error('invalid listId');
-    }
-
-    const list = await this.getListByIdUsecase.execute(listId);
-    return list.coupleId === coupleId;
-  }
-
-  private async checkMultipleContents(
-    coupleId: string,
-    contentIds: string[],
-  ): Promise<boolean> {
+  async canEditContents(coupleId: string, contentIds: string[]): Promise<void> {
     const contents = await this.getContentsByIdsUsecase.execute(contentIds);
     const listId = contents[0]?.listId;
 
-    const sameListIds = contents.every((content) => content.listId === listId);
+    const isSameListId = contents.every((content) => content.listId === listId);
 
-    if (!listId || !sameListIds) {
-      throw new Error('invalid listId');
-    }
+    if (!listId || !isSameListId) throw new Error('invalid content ids');
 
     const list = await this.getListByIdUsecase.execute(listId);
-    return list.coupleId === coupleId;
+    if (list.coupleId !== coupleId) throw new UnauthorizedException();
   }
 }
