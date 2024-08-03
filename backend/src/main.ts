2b5d417fa67be4e4f7bccc81f8457732ec1b1f19
exchange-wata <exchange.wata0907@gmail.com>
2024-08-03 16:42:20 +0900

    修正

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.DATABASE_URL);
-
   await app.listen(3000);
 }
 bootstrap();
